6c35f02ccf214e3e9a4311ffb50fd09a1960a336
Terry <55205907+softerry@users.noreply.github.com>
2024-09-03 20:27:28 +0100

    Styling the login form

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './style.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 // import Layout from './pages/Layout';
 // import Home from "./pages/Home";
 
@@ -24,17 +26,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //login form
 function LoginForm(){
-    return (<form action="">
-      <h1>Signin</h1>
-     <input type="text" placeholder="Email"></input>
-      <input type="password" placeholder="Password"></input>
-     <label><input type="checkbox"></input>Remember me</label> 
-      <a href='#'>Forget Password</a>
-      <input type="submit" value="Sign In"></input>
-    </form>)
+  return (<form action="">
+    <h1>Login</h1>
+    <div class="social-container">
+    <a href="#" class="social-icon"><FontAwesomeIcon icon={(faGoogle)}/></a>
+    <a href="#" class="social-icon"><FontAwesomeIcon icon={(faFacebook)}/></a>
+    </div>
+   <input type="text" placeholder="Email" required></input>
+    <input type="password" placeholder="Password" required></input>
+   <label><input type="checkbox"></input>Remember me</label> 
+    <a href='#' class="forget-password">Forget Password</a>
+    <input type="submit" value="Sign In"></input>
+
+  </form>)
   }
-
-
+  
   // render sigin form to html
   const loginForm = ReactDOM.createRoot(document.querySelector('#loginForm'));
   loginForm.render(
