860a3e98c2d936495787c7bcc766ab9947049327
Terry <55205907+softerry@users.noreply.github.com>
2024-09-07 19:46:27 +0100

    show and hide password icon

--- src/index.js
+++ src/index.js
@@ -1,46 +1,66 @@
+//components
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
-// import Layout from './pages/Layout';
-// import Home from "./pages/Home";
+import { faEye } from '@fortawesome/free-regular-svg-icons';
+import { faEyeSlash } from '@fortawesome/free-regular-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 
-// export default function app(){
-//   return (
-//     <BrowserRouter>
-//     <Routes>
-//       <Route path='/' element={<Layout/>}>
-//       <Route  index element= {<Home />} />
-//       <Route path="Loginform" element={<Loginform/>}/>
-//       </Route>
-//     </Routes>
-//     </BrowserRouter>
-//   )
-// }
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<app/>);
+
+
 
 //login form
 function LoginForm(){
+   // create three states
+const [password, setPassword] = useState("");
+const [type, setType] = useState('password');
+const [icon, setIcon] = useState(faEyeSlash);
+
+const PasswordToggle = () =>{
+  // function that handle password(eye off icon) and show password (eye icon)
+   if(type ==='password'){
+     setIcon(faEye);
+     setType('text')
+   }else{
+     setIcon(faEyeSlash)
+     setType('password');
+   }
+  }
+  
   return (<form action="">
     <h1>Login</h1>
     <div class="social-container">
     <a href="#" class="social-icon"><FontAwesomeIcon icon={(faGoogle)}/></a>
     <a href="#" class="social-icon"><FontAwesomeIcon icon={(faFacebook)}/></a>
     </div>
-   <input type="text" placeholder="Email" required></input>
-    <input type="password" placeholder="Password" required></input>
-   <label><input type="checkbox"></input>Remember me</label> 
+    <div class="input-icons">
+    <span class="icon"><FontAwesomeIcon icon={(faEnvelope)}/></span>
+    <input type="text" placeholder="Email" required></input>
+    </div>
+{/* onclick function is applied to the span element and also access the icon to icon instand of putting faEye or faEyeslash */}
+  <div class="input-icons">
+  <span class="icon"><FontAwesomeIcon icon={icon} onClick={PasswordToggle}/></span>
+    {/* // create event to change the password to text  */}
+      <input class="password-icon" type={type} placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete='password'required></input>
+    
+    
+   </div>
+
+    <div class="submit-container">
+    <label><input type="checkbox"></input>Remember me</label> 
     <a href='#' class="forget-password">Forget Password</a>
     <input type="submit" value="Sign In"></input>
-
+    </div>
+    
   </form>)
   }
-  
+ 
   // render sigin form to html
   const loginForm = ReactDOM.createRoot(document.querySelector('#loginForm'));
   loginForm.render(
---
+++ src/pages/signupform.js
@@ -0,0 +1,14 @@
+// export default function app(){
+//   return (
+//     <BrowserRouter>
+//     <Routes>
+//       <Route path='/' element={<Layout/>}>
+//       <Route  index element= {<Home />} />
+//       <Route path="Loginform" element={<Loginform/>}/>
+//       </Route>
+//     </Routes>
+//     </BrowserRouter>
+//   )
+// }
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<app/>);
